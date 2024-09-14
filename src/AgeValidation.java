b0218a30c2552fce287621636a7af2cf14dc2e5f
ValeWave <jelinarevalo@gmail.com>
2024-09-14 07:53:17 -0500

    Refactor complete for quiz

--- src/AgeValidation.java
+++ src/AgeValidation.java
@@ -1,13 +1,21 @@
+import java.util.Scanner;
 public class AgeValidation {
+    
     public static void main(String[] args) {
-        int age = 18;
+        Scanner scanner = new Scanner(System.in);
+        final int AGE_VALIDATION = 18;
+        // Se crea una constante la cual será la edad fija para tener acceso al programa
+            System.out.println("Ingresa tu edad");
+            int age = scanner.nextInt();
         
-        if (age >= 18) {
+        
+        if (age >= AGE_VALIDATION) {
             System.out.println("Access granted");
-        }
-        
-        if (age < 18) {
+            //si el dato ingresado es mayor o igual a 18 se ejecutará este comando
+        } else {
             System.out.println("Access denied");
+            //si el dato ingresado es menor a 18 se ejecutará este comando
         }
     }
+
 }
--- src/Store.java
+++ src/Store.java
@@ -1,12 +1,12 @@
 public class Store {
     public static void main(String[] args) {
-        int p1 = 15;
-        int p2 = 10;
-        int p3 = 5;
+        int productPrice1 = 15;
+        int productPrice2 = 10;
+        int productPrice3 = 5;
 
-        int total1 = p1 * 2;
-        int total2 = p2 * 3;
-        int total3 = p3 * 4;
+        int total1 = productPrice1 * 2;
+        int total2 = productPrice2 * 3;
+        int total3 = productPrice3 * 4;
 
         int totalSales = total1 + total2 + total3;
 
